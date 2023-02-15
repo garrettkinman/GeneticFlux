5dca07b168304a11ed80038bcb6e2e645b0140a8
garrettkinman <garrettkinman@gmail.com>
2023-02-14 20:50:26 -0500

    laid out skeleton of algorithm

--- neuroevolver.jl
+++ neuroevolver.jl
@@ -4,12 +4,14 @@ using Flux
 using Flux.Data: DataLoader
 using Flux: onehotbatch, onecold, logitcrossentropy
 using Parameters: @with_kw
+using MLDatasets
 using MLDatasets: MNIST
 using Zygote
 using CUDA
 using Pipe
 using ProgressLogging
 using BenchmarkTools
+using InteractiveUtils
 
 ## prepare MNIST data
 
@@ -43,7 +45,8 @@ end
 function build_model(; imgsize=(28,28,1), nclasses=10)
     return Chain(
  	    Dense(prod(imgsize), 32, relu),
-            Dense(32, nclasses))
+        Dense(32, nclasses)
+    )
 end
 
 function loss_all(dataloader, model)
@@ -66,16 +69,43 @@ end
     Genetic(popsize = 64, ϵ = 1)
 
 Genetic algorithm-based optimizer with population size for each generation of `popsize`, and `ϵ` survivors of each generation.
-
 """
-mutable struct Genetic <: Flux.Optimise.AbstractOptimiser
+@with_kw mutable struct Genetic <: Flux.Optimise.AbstractOptimiser
     popsize::Int32 = 64
     ϵ::Int32 = 1
 end
 
 function apply!(o::Genetic, x, Δ)
+    # return a Δ for each element of x (which is a subset of model params)
+    # the calling funtion will call x .-= apply!(opt, x, x̄r), where x̄r is just the gradients corresponding to x
+    # treat Δ as all zeros to start with, as we will zero-initialize them to feed in,
+    # because this package was designed with gradient-based methods in mind, and thus requires you feed in gradients (even if not real)
+
     # TODO
-  end
+    # randomly generate a population of Δ candidates (of size o.popsize)
+    # perform a feed-through the network for each candidate Δ
+    # select the surviving Δ based on minimized loss
+
+end
+
+
+"""
+Functions like Zygote.gradient and returns a delta for how much each given param should change.
+Unlike Zygote.gradient, it doesn't calculate any gradients. Instead, it generates a population of mutations,
+runs the given training data through each mutated variant of the model, and selects the best performing mutation(s) to return.
+"""
+function evolve(opt::Genetic, ps::Zygote.Params, f::Function, data)
+    # TODO: generate population of mutations
+        # store a temporary deep copy of the current params
+        # for each mutant
+            # randomly mutate a subset of the params
+            # run the model over the data
+            # if lowest loss of all the mutants so far, keep
+        # restore the original params to the model
+        # return the best-performing mutant
+    # apply each mutation to the model
+    # calculate loss for each mutant
+end
 
 function train!()
     args = Args()
@@ -85,11 +115,50 @@ function train!()
     train_data = train_data .|> args.device
     test_data = test_data .|> args.device
 
+    loss(x,y) = logitcrossentropy(model(x), y)
 
+    ## Training
+    evalcb = () -> @show(loss_all(train_data, model))
+    opt = ADAM()
+
+    # @epochs args.epochs Flux.train!(loss, params(model), train_data, opt, cb = evalcb)
+    ps = Flux.params(model)
+
+    @withprogress for (i, (x, y)) ∈ enumerate(train_data)
+        # gradients = gradient(() -> loss(x, y), params)
+        mutations = evolve(opt, ps, loss, train_data)
+        # println(typeof(gradients))
+        # Flux.Optimise.update!(opt, ps, gradients) # <-- need to target this
+        Flux.Optimise.update!(opt, ps, mutations)
+        @logprogress i / length(enumerate(train_loader))
+    end
 
 end
 
 ## run
+
+args = Args()
+model = build_model()
+ps = Flux.params(model)
+train_data, test_data = getdata(args)
+
+for (i, (x,y)) in enumerate(train_data)
+    @show size.((x, y))
+    gradients = gradient(() -> logitcrossentropy(model(x), y), ps)
+    @show ps |> length
+    for p in ps
+        @show p |> size
+        @show gradients[p] |> size
+        break
+    end
+    break
+end
+
+Zygote.Params
+methodswith(Zygote.Grads)
+methodswith(Zygote.Params)
+
+train!()
 
 Dense(prod((28,28,1)), 10).weight
 
